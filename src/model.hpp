901d26a29a76261cde984c294b4d917aea9edc7d
Jayden E <aneeshedara@gmail.com>
2024-07-14 00:21:18 -0400

    Sunset AVX buffers; write base model class

--- src/model.hpp
+++ src/model.hpp
@@ -3,21 +3,32 @@
 #include <vector>
 #include <cstdint>
 #include "jtxlib/math/vec2.hpp"
+#include "jtxlib/math/vec3.hpp"
 
 namespace jswr {
-    struct VertexBuffer {
-        std::vector<jtx::AVXVec3f> x, y, z;
-        std::vector<jtx::AVXVec3f> nx, ny, nz;
-        std::vector<jtx::Vec2f> u, v;
+    struct Vertex {
+        jtx::Vec3f position;
+        jtx::Vec3f normal;
+        jtx::Vec2f uv;
     };
 
-    struct IndexBuffer {
-        std::vector<uint32_t> indices;
+    struct Face {
+        uint32_t v0, v1, v2;
     };
 
-    class model {
-
+    struct Mesh {
+        uint32_t vertex_offset, num_vertices;
+        uint32_t index_offset, index_count;
     };
 
+    class Model {
+    public:
+        std::vector<Vertex> vertexBuffer;
+        std::vector<Face> indexBuffer;
+        std::vector<Mesh> meshes;
+
+        Model() = default;
+        ~Model() = default;
+    };
 } // jswr
 
