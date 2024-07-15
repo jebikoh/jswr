fb93b87198c6e29b37646df536ea2b196bce81f9
Jayden E <aneeshedara@gmail.com>
2024-07-15 17:22:46 -0400

    Add basic model loader; add assimp and stb_image.h

--- src/model.cpp
+++ src/model.cpp
@@ -1,8 +1,164 @@
-//
-// Created by Aneesh on 7/12/2024.
-//
-
+// model.cpp
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include "model.hpp"
 
+#include <jtxlib/math/vec3.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 namespace jswr {
+    //region Helper functions
+    static jtx::Mat4 aiMatrix4x4ToMat4(const aiMatrix4x4 &m) {
+        return {
+            m.a1, m.a2, m.a3, m.a4,
+            m.b1, m.b2, m.b3, m.b4,
+            m.c1, m.c2, m.c3, m.c4,
+            m.d1, m.d2, m.d3, m.d4
+        };
+    }
+
+    static jtx::Vec3f getMaterialVec3(aiMaterial *material, const char *key, unsigned int type, unsigned int index) {
+        aiColor3D color;
+        if (material->Get(key, type, index, color) == AI_SUCCESS) {
+            return {color.r, color.g, color.b};
+        }
+        return {-1.0f, -1.0f, -1.0f};
+    }
+
+    static float getMaterialFloat(aiMaterial *material, const char *key, unsigned int type, unsigned int index) {
+        float value;
+        if (material->Get(key, type, index, value) == AI_SUCCESS) {
+            return value;
+        }
+        return -1.0f;
+    }
+    //endregion
+
+    static bool loadTexture(Model &model, aiMaterial *material, aiTextureType type, std::vector<uint32_t> &textures) {
+        // Texture data is stored in the model
+        // Texture IDs (their index in the buffer) are stored in the material
+        // When adding a texture, we check if the path is already in the buffer, and just return the ID if so.
+        for (unsigned int i = 0; i < material->GetTextureCount(type); ++i) {
+            aiString str;
+            material->GetTexture(type, i, &str);
+            bool loaded = false;
+            for (uint32_t j = 0; j < textures.size(); ++j) {
+                if (std::strcmp(model.textureBuffer[j].path.data(), str.C_Str()) == 0) {
+                    textures.push_back(j);
+                    loaded = true;
+                    break;
+                }
+            }
+            if (!loaded) {
+                Texture tex;
+                std::string filename = str.C_Str();
+
+                int width, height, channels;
+                unsigned char *data = stbi_load(filename.c_str(), &width, &height, &channels, 0);
+                if (!data) {
+                    std::cerr << "Error: Failed to load texture " << filename << std::endl;
+                    return false;
+                }
+                tex.data = std::vector<uint8_t>(data, data + width * height * channels);
+                tex.width = width;
+                tex.height = height;
+                tex.channels = channels;
+                tex.type = static_cast<TextureType>(type);
+                tex.path = filename;
+
+                auto texID = static_cast<uint32_t>(model.textureBuffer.size());
+                model.textureBuffer.push_back(tex);
+                textures.push_back(texID);
+            }
+        }
+        return true;
+    }
+
+    static bool loadMaterials(const aiScene *scene, Model &model) {
+        model.materials.reserve(scene->mNumMaterials);
+
+        for (unsigned int i = 0; i < scene->mNumMaterials; ++i) {
+            aiMaterial *material = scene->mMaterials[i];
+            Material m;
+
+            m.ambient   = getMaterialVec3(material, AI_MATKEY_COLOR_AMBIENT);
+            m.diffuse   = getMaterialVec3(material, AI_MATKEY_COLOR_DIFFUSE);
+            m.specular  = getMaterialVec3(material, AI_MATKEY_COLOR_SPECULAR);
+            m.shininess = getMaterialFloat(material, AI_MATKEY_SHININESS);
+
+            if (!loadTexture(model, material, aiTextureType_DIFFUSE, m.diffuse_maps)) return false;
+            if (!loadTexture(model, material, aiTextureType_SPECULAR, m.specular_maps)) return false;
+            if (!loadTexture(model, material, aiTextureType_NORMALS, m.normal_maps)) return false;
+            if (!loadTexture(model, material, aiTextureType_HEIGHT, m.height_maps)) return false;
+
+            model.materials.emplace_back(m);
+        }
+
+        return true;
+    }
+
+    static Mesh processMesh(aiMesh *mesh, Model &model, const aiScene *scene, jtx::Mat4 &transform) {
+        Mesh m;
+        m.vertex_offset = static_cast<uint32_t>(model.vertexBuffer.size());
+        m.index_offset = static_cast<uint32_t>(model.indexBuffer.size());
+        m.model_matrix = transform;
+        m.material_id = mesh->mMaterialIndex;
+
+        // Process vertices
+        for (unsigned int i = 0; i < mesh->mNumVertices; ++i) {
+            Vertex v;
+            v.position = {mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z};
+            v.normal = {mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z};
+            if (mesh->mTextureCoords[0]) {
+                v.uv = {mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y};
+            } else {
+                v.uv = {0.0f, 0.0f};
+            }
+            model.vertexBuffer.push_back(v);
+        }
+        // Process indices
+        for (unsigned int i = 0; i < mesh->mNumFaces; ++i) {
+            aiFace face = mesh->mFaces[i];
+            if (face.mNumIndices == 3) {
+                model.indexBuffer.push_back({face.mIndices[0], face.mIndices[1], face.mIndices[2]});
+            } else {
+                std::cerr << "Warning: Skipping on-triangular face in mesh" << std::endl;
+            }
+        }
+
+        m.num_vertices = mesh->mNumVertices;
+        m.index_count = static_cast<uint32_t>(model.indexBuffer.size() - m.index_offset);
+
+        return m;
+    }
+
+    static void processNode(aiNode *node, const aiScene *scene, Model &model, const jtx::Mat4 &pTransform) {
+        jtx::Mat4 transform = pTransform * aiMatrix4x4ToMat4(node->mTransformation);
+
+        for (unsigned int i = 0; i < node->mNumMeshes; ++i) {
+            aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+            processMesh(mesh, model, scene, transform);
+        }
+
+        for (unsigned int i = 0; i < node->mNumChildren; ++i) {
+            processNode(node->mChildren[i], scene, model, transform);
+        }
+    }
+
+    bool loadModel(const char *path, Model &model) {
+        Assimp::Importer importer;
+        const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenSmoothNormals);
+
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+            std::cerr << "Error: " << importer.GetErrorString() << std::endl;
+            return false;
+        }
+
+        if(!loadMaterials(scene, model)) return false;
+        processNode(scene->mRootNode, scene, model, jtx::Mat4::identity());
+
+        return true;
+    }
 } // jswr
--- src/model.hpp
+++ src/model.hpp
@@ -1,8 +1,13 @@
+// model.hpp
 #pragma once
+
 #include <vector>
 #include <cstdint>
+#include <assimp/material.h>
+
 #include "jtxlib/math/vec2.hpp"
 #include "jtxlib/math/vec3.hpp"
+#include "jtxlib/math/mat4.hpp"
 
 namespace jswr {
     struct Vertex {
@@ -15,19 +20,47 @@ namespace jswr {
         uint32_t v0, v1, v2;
     };
 
+    enum class TextureType {
+        DIFFUSE = aiTextureType_DIFFUSE,
+        SPECULAR = aiTextureType_SPECULAR,
+        NORMAL = aiTextureType_NORMALS,
+        HEIGHT = aiTextureType_HEIGHT
+    };
+
+    struct Texture {
+        uint32_t width, height, channels;
+        TextureType type;
+        std::string path;
+        std::vector<uint8_t> data;
+    };
+
+    struct Material {
+        jtx::Vec3f ambient;
+        jtx::Vec3f diffuse;
+        jtx::Vec3f specular;
+        float shininess;
+        std::vector<uint32_t> diffuse_maps;
+        std::vector<uint32_t> specular_maps;
+        std::vector<uint32_t> normal_maps;
+        std::vector<uint32_t> height_maps;
+    };
+
     struct Mesh {
         uint32_t vertex_offset, num_vertices;
         uint32_t index_offset, index_count;
+        // Material ID is its index in the materials array.
+        uint32_t material_id;
+        jtx::Mat4 model_matrix;
     };
 
-    class Model {
-    public:
+    struct Model {
         std::vector<Vertex> vertexBuffer;
         std::vector<Face> indexBuffer;
+        std::vector<Texture> textureBuffer;
+        std::vector<Material> materials;
         std::vector<Mesh> meshes;
-
-        Model() = default;
-        ~Model() = default;
     };
+
+    bool loadModel(const char *path, Model &model);
 } // jswr
 
